f7853ff66f2f08188c5bfb181c0cc42a359a9824
alwil17 <willialfred24@gmail.com>
2025-04-01 03:06:51 +0000

    fixed pytest error

--- app/api/auth.py
+++ app/api/auth.py
@@ -14,9 +14,9 @@ router = APIRouter(tags=["Authentication"])
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.now(datetime.timezone.utc) + expires_delta
+        expire = datetime.now() + expires_delta
     else:
-        expire = datetime.now(datetime.timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.now() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.APP_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
     return encoded_jwt
